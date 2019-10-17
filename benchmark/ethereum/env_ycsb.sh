1debc742a2a7ecac92c8cac04c098b14057d1de1
ppk212 <ppk212@naver.com>
2019-10-17 07:16:40 +0000

    edit path for donothing experiment

--- benchmark/ethereum/env_ycsb.sh
+++ benchmark/ethereum/env_ycsb.sh
@@ -1,11 +1,8 @@
-ETH_HOME=/users/dinhtta/anh/blockchain-perf/benchmark/ethereum
+ETH_HOME=/home/samuel/blockbench/benchmark/ethereum
 HOSTS=$ETH_HOME/hosts
 CLIENTS=$ETH_HOME/clients
-ETH_DATA=/data/dinhtta/eth
+ETH_DATA=/data/samuel/eth
 LOG_DIR=$ETH_HOME/donothing_ycsb
-EXE_HOME=$ETH_HOME/../../src/ycsb
+EXE_HOME=/home/samuel/blockbench/benchmark/src/donothing
 BENCHMARK=ycsb
 
-##comment these out for smallbank
-#EXE_HOME=$ETH_HOME/../../src/smallbank/ethereum/ycsb
-#BENCHMARK=smallbank
