77c826f1acf3b0079d241a1d4a165696ef6d31b4
root <ppk212@naver.com>
2019-10-29 07:08:35 +0000

    edit path

--- benchmark/ethereum/env.sh
+++ benchmark/ethereum/env.sh
@@ -1,8 +1,8 @@
-ETH_HOME=/home/samuel/blockbench/benchmark/ethereum
+ETH_HOME=/home/ubuntu/blockbench/benchmark/ethereum
 HOSTS=$ETH_HOME/hosts
 CLIENTS=$ETH_HOME/clients
-ETH_DATA=/data/samuel/eth
+ETH_DATA=/data/ubuntu/eth
 LOG_DIR=$ETH_HOME/donothing_ycsbresults_1
-EXE_HOME=/home/samuel/blockbench/benchmark/src/macro/kvstore
+EXE_HOME=/home/ubuntu/blockbench/benchmark/src/macro/kvstore
 BENCHMARK=ycsb
 
